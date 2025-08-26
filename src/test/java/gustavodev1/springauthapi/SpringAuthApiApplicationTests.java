7608421b30577514156baf825ca59ed0e113b623
GustavoDev1 <gustavo.fagundes@hubens.com.br>
2025-08-26 12:54:13 -0300

    chore: initial Spring Boot project setup

---
+++ src/test/java/gustavodev1/springauthapi/SpringAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavodev1.springauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
